eb0396b0a831d4af46f53c65072b8acb87290cf9
James Darling <james@abscond.org>
2012-01-20 10:50:08 +0000

    Config validation and default difficulty

--- publication.rb
+++ publication.rb
@@ -4,6 +4,21 @@ require 'json'
 post '/pull' do
   require './sudoku_generator'
   config = JSON.parse(params[:config])
+  config['difficulty'] = "easy" if config['test']
   @puzzle_out = SudokuGenerator.new(config['difficulty'].to_sym).generate.split(%r{\s*}).each_slice(3).to_a
   erb :puzzle
+end
+
+post '/validate_config' do
+  content_type :json
+  response = {}
+  response[:errors] = []
+  config = JSON.parse(params[:config])
+  if ["easy", "medium", "hard"].include?(config['difficulty'])
+    response[:valid] = true
+  else
+    response[:valid] = false
+    response[:errors] << "Difficulty must be easy, medium or hard"
+  end
+  response.to_json
 end
